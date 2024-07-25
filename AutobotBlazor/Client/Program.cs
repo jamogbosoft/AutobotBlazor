92bc8e45a0318922e5ef52d17e75140e14c4e609
Ogbonna, James Chinyere <jamogbosoft@gmail.com>
2024-07-25 10:28:28 +0100

    Twenty Fift July Commit

--- AutobotBlazor/Client/Program.cs
+++ AutobotBlazor/Client/Program.cs
@@ -3,6 +3,8 @@ global using AutobotBlazor.Client.Service.CandidateService;
 global using AutobotBlazor.Client.Service.CentreService;
 global using AutobotBlazor.Client.Service.ExamService;
 global using AutobotBlazor.Shared.Models;
+global using Microsoft.AspNetCore.Components.Authorization;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
